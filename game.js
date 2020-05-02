a26106fcd7797785813f8b42c61a43738255fc55
Abhishek <gmabh07@gmail.com>
2020-05-02 15:23:40 +0530

    1st commit after merging

--- game.js
+++ game.js
@@ -664,7 +664,6 @@ Asteroid = function () {
   this.solid = true;
   this.visible = true;
   this.scale = 6;
-  this.onevariabel=false;
   this.postMove = this.wrapPostMove;
   this.premove=false;
 
